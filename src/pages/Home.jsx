4f14fe41aba313ab6d691a67b6a694b796130dac
AdamSmith <omercoskun4369@gmail.com>
2024-04-21 01:59:17 +0300

    added animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Carousell />
       <a href="https://wa.me/905433533310" target="_blank">
         {" "}
-        <IoLogoWhatsapp className="fixed top-28 right-5 md:top-28 md:right-10 text-[#65e44c]  cursor-pointer hover:text-[#7FFF00] text-7xl" />
+        <IoLogoWhatsapp className="fixed top-28 right-5 animate-pulse md:animate-none  md:top-28 md:right-10 text-[#65e44c]  cursor-pointer hover:text-[#7FFF00] text-7xl" />
       </a>
     </div>
   );
